a6d4b9d3aed0a9cfcb5906e4a2c6f07cff932a97
Marcus Yim <marcusyim@outlook.com>
2022-09-16 15:43:08 -0700

    hahahaha

--- StockSimulation/Form1.cs
+++ StockSimulation/Form1.cs
@@ -1,32 +1,29 @@
-﻿using LiveCharts.Wpf;
-using LiveCharts;
-using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
+﻿using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Media;
 using System.Windows.Controls;
 using System.Windows.Forms;
+using LiveCharts.Wpf;
+using LiveCharts;
+using System.Collections.Generic;
+using System;
 
 namespace StockSimulation
 {
-    public partial class BollingerPlot : Form
+    public partial class Form1 : Form
     {
-        public BollingerPlot()
+        public Form1()
         {
             InitializeComponent();
         }
 
-        private void BollingerPlot_Load(object sender, EventArgs e)
+        private void Form1_Load(object sender, EventArgs e)
         {
             ColumnSeries col = new ColumnSeries() { DataLabels = true, Values = new ChartValues<int>(), LabelPoint = point => point.Y.ToString() };
             Axis ax = new Axis() { Separator = new LiveCharts.Wpf.Separator() { Step = 1, IsEnabled = false } };
             ax.Labels = new List<String>();
-
         }
     }
 }
